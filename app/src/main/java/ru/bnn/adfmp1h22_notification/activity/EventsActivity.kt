460a6c2c8ac460ffb563b9a0ef67a5ecf4e5ee35
Антон Пушпышев <an.pushpyshev@gmail.com>
2022-04-17 22:21:09 +0300

    added activity events front

--- app/src/main/java/ru/bnn/adfmp1h22_notification/activity/EventsActivity.kt
+++ app/src/main/java/ru/bnn/adfmp1h22_notification/activity/EventsActivity.kt
@@ -1,10 +1,12 @@
 package ru.bnn.adfmp1h22_notification.activity
 
 import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
-import androidx.appcompat.app.AppCompatActivity
+import android.view.View
+import ru.bnn.adfmp1h22_notification.MainActivity
 import ru.bnn.adfmp1h22_notification.R
 
 class EventsActivity : AppCompatActivity() {
@@ -17,6 +19,20 @@ class EventsActivity : AppCompatActivity() {
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.main_menu, menu)
         return true
+    }
+
+    fun editEventOnClick(view : View) {
+        val intent = Intent(this, EventSettingsActivity::class.java)
+        this.startActivity(intent)
+    }
+
+    fun deleteEventOnClick(view : View){
+        val intent = Intent(this, MainActivity::class.java)
+        val bundle = Bundle()
+        bundle.putString("alert_delete", "alert_delete")
+        bundle.putString("text", "Удалить событие?")
+        intent.putExtras(bundle)
+        startActivity(intent)
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
