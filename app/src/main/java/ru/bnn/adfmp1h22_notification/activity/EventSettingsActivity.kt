fba3999d8f40b2c70517b87080c8a3f70d237a23
Akibardin <akibardin@list.ru>
2022-04-17 22:04:57 +0300

    add EventSettingsActivity.kt

---
+++ app/src/main/java/ru/bnn/adfmp1h22_notification/activity/EventSettingsActivity.kt
@@ -0,0 +1,18 @@
+package ru.bnn.adfmp1h22_notification.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import ru.bnn.adfmp1h22_notification.R
+
+class EventSettingsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        supportActionBar?.title = "Редактирование события"
+        setContentView(R.layout.activity_event_settings)
+    }
+
+    fun goHome(view: View) {
+    }
+
+}
