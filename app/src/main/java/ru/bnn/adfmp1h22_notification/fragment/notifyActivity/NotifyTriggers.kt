027af4e5b0e2fe278ed6577d8230aad579e4b236
Akibardin <akibardin@list.ru>
2022-04-17 22:01:54 +0300

    update NotifyTriggers.kt

--- app/src/main/java/ru/bnn/adfmp1h22_notification/fragment/notifyActivity/NotifyTriggers.kt
+++ app/src/main/java/ru/bnn/adfmp1h22_notification/fragment/notifyActivity/NotifyTriggers.kt
@@ -7,27 +7,10 @@ import android.view.View
 import android.view.ViewGroup
 import ru.bnn.adfmp1h22_notification.R
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [NotifyTriggers.newInstance] factory method to
- * create an instance of this fragment.
- */
 class NotifyTriggers : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
     }
 
     override fun onCreateView(
@@ -39,22 +22,10 @@ class NotifyTriggers : Fragment() {
     }
 
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment NotifyTriggers.
-         */
-        // TODO: Rename and change types and number of parameters
+
         @JvmStatic
-        fun newInstance(param1: String, param2: String) =
+        fun newInstance() =
             NotifyTriggers().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
             }
     }
 }
