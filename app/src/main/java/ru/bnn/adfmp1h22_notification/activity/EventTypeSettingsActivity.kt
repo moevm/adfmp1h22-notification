7a4fed6d1547e8a807ad1ea5c9489e1eba80773b
Artem Danilov <danilov.arrtem@gmail.com>
2022-04-17 21:33:57 +0300

    add EventTypeSettingsActivity

---
+++ app/src/main/java/ru/bnn/adfmp1h22_notification/activity/EventTypeSettingsActivity.kt
@@ -0,0 +1,41 @@
+package ru.bnn.adfmp1h22_notification.activity
+
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import ru.bnn.adfmp1h22_notification.R
+import ru.bnn.adfmp1h22_notification.activity.databinding.ActivityEventTypeSettingsBinding
+
+class EventTypeSettingsActivity : AppCompatActivity() {
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var binding: ActivityEventTypeSettingsBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityEventTypeSettingsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
+
+        val navController = findNavController(R.id.nav_host_fragment_content_event_type_settings)
+        appBarConfiguration = AppBarConfiguration(navController.graph)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+
+        binding.fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+        }
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment_content_event_type_settings)
+        return navController.navigateUp(appBarConfiguration)
+                || super.onSupportNavigateUp()
+    }
+}
